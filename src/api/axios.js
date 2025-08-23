03b9f2787223e187d2af40ce24fdf8ed204a2107
Jay <jay1904prajapati@gmail.com>
2025-08-23 19:18:06 +0530

    add more change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shop-ease-backend-seven.vercel.app",
+  baseURL: "https://shop-ease-backend-seven.vercel.app/api",
   withCredentials: true,
 });
 
