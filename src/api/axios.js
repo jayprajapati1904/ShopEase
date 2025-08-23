093760f0aa8623317e55daea7dda399f8b5dfbc8
Jay <jay1904prajapati@gmail.com>
2025-08-23 19:03:02 +0530

    add bakcend url in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://shop-ease-backend-seven.vercel.app/",
   withCredentials: true,
 });
 
